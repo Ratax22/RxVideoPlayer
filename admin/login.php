7c4ee5683cd7dc104f977c91a43f53d886b685a6
Ratax <alanphilipballarino@gmail.com>
2026-03-19 18:39:19 -0300

    -- Test login  --

--- admin/login.php
+++ admin/login.php
@@ -3,7 +3,8 @@ session_start();
 require_once '../config.php';
 
 if (isset($_SESSION['usuario_id'])) {
-    header("Location: index.php");
+    //header("Location: index.php");
+    echo "Usurio esta definido";
     exit;
 }
 
