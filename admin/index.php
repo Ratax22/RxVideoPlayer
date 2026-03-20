96f755684dd83255007047cdb85c60ec5a9507e2
Ratax <alanphilipballarino@gmail.com>
2026-03-19 22:34:59 -0300

    -- Pasamos todos los modulso por hub central  --

--- admin/index.php
+++ admin/index.php
@@ -19,7 +19,7 @@ $menu_items = [
     'usuarios'   => ['label' => 'Usuarios',      'icon' => 'bi-people',         'roles' => ['admin','dueño','supervisor']],
     'usuario_edit'   => ['label' => 'Usuarios Editar',      'icon' => 'bi-people',         'roles' => ['admin','dueño','supervisor']],
     'empresas'   => ['label' => 'Empresas',      'icon' => 'bi-building',       'roles' => ['admin','dueño']],
-    'empresa_editar'   => ['label' => 'Empresas Editar',      'icon' => 'bi-building',       'roles' => ['admin','dueño']],
+    'empresa_edit'   => ['label' => 'Empresas Editar',      'icon' => 'bi-building',       'roles' => ['admin','dueño']],
     'sucursales' => ['label' => 'Sucursales',    'icon' => 'bi-shop',           'roles' => ['admin','dueño']],
     'sucursal_edit' => ['label' => 'Sucursales Editar',    'icon' => 'bi-shop',           'roles' => ['admin','dueño']],
 ];
