288d6695c9a2679992fd0088db1941888309d3ea
Ratax <alanphilipballarino@gmail.com>
2026-03-19 22:19:58 -0300

    -- Pasamos todos los modulso por hub central  --

--- admin/index.php
+++ admin/index.php
@@ -80,12 +80,10 @@ if (!isset($menu_items[$action]) || !in_array($rol, $menu_items[$action]['roles'
     <main class="content col-12 col-lg-10 p-4">
         <?php
         // Router simple: incluir la sección según action
-        echo __DIR__ . '/' . $action . '_section.php';
         $section_file = __DIR__ . '/' . $action . '_section.php';
         if (file_exists($section_file)) {
             include $section_file;
         } else {
-            echo $section_file;
             echo '<div class="alert alert-warning">Sección no encontrada: ' . htmlspecialchars($action) . '</div>';
         }
         ?>
